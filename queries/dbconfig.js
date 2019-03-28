8768328fc8bb580a4c8694cf771fe71179ee8427
markomonsalud <mfgmonsalud@gmail.com>
2019-03-29 02:27:22 +0800

    Fixed directories and added database config file

---
+++ queries/dbconfig.js
@@ -0,0 +1,15 @@
+var dbconfig = {
+    database: {
+        host:      'localhost',
+        user:       'root',
+        password: 'root',
+        port:       3306,
+        db:       'inventory'
+    },
+    server: {
+        host: '127.0.0.1',
+        port: '3000'
+    }
+}
+ 
+module.exports = dbconfig
