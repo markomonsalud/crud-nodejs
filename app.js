afb3120896e9ecf47b0ff60f25c98da2f0e96d32
markomonsalud <mfgmonsalud@gmail.com>
2019-03-29 09:22:30 +0800

    Fixed bugs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-var mysql = require('mysql') 
+var mysql = require('mysql')
 
 var myConnection  = require('express-myconnection')
 
@@ -10,10 +10,10 @@ var dbOptions = {
     host:      config.database.host,
     user:       config.database.user,
     password: config.database.password,
-    port:       config.database.port, 
+    port:       config.database.port,
     database: config.database.db
 }
- 
+
 app.use(myConnection(mysql, dbOptions, 'pool'))
 
 app.set('view engine', 'ejs')
@@ -23,7 +23,7 @@ var items = require('./routes/items')
 
 var expressValidator = require('express-validator')
 app.use(expressValidator())
- 
+
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -31,7 +31,7 @@ app.use(bodyParser.json())
 var flash = require('express-flash')
 var session = require('express-session')
 app.use(flash())
-app.use(session({ 
+app.use(session({
     secret: 'ninja',
     resave: false,
     saveUninitialized: true,
@@ -39,6 +39,6 @@ app.use(session({
 app.use('/', index)
 app.use('/items', items)
 
-app.listen(3000, function(){
-    console.log('Server running at port 3000: http://127.0.0.1:3000')
+app.listen(3000, '0.0.0.0', function(){
+    console.log('Server running at port 3000: http://0.0.0.0:3000')
 })
--- routes/items.js
+++ routes/items.js
@@ -6,14 +6,15 @@ app.get('/', function(req, res, next) {
     req.getConnection(function(error, conn) {
         conn.query('SELECT * FROM items ORDER BY id DESC',function(err, rows, fields) {
             if (err) {
+                console.log(err);
                 req.flash('error', err)
                 res.render('items/list', {
-                    title: 'Items List', 
+                    title: 'Items List',
                     data: ''
                 })
             } else {
                 res.render('items/list', {
-                    title: 'Items List', 
+                    title: 'Items List',
                     data: rows
                 })
             }
@@ -24,62 +25,65 @@ app.get('/', function(req, res, next) {
 //Add Item
 app.get('/add', function(req, res, next){
     res.render('items/add', {
-        name: 'Add New Item',
+        name: 'New Item',
         quantity: '',
         amount: '',
         title: ''
     })
 })
 
-app.post('/add', function(req, res, next){    
+app.post('/add', function(req, res, next){
+    console.log(req.body);
     req.assert('name', 'Name is required').notEmpty()
     req.assert('quantity', 'quantity is required').notEmpty()
-    req.assert('amount', 'A valid amount is required').isEmail()
- 
+    req.assert('amount', 'A valid amount is required').notEmpty()
+
     var errors = req.validationErrors()
-    
+
+    console.log('errors', errors);
+
     if( !errors ) {
-        
- 
         var Item = {
             name: req.sanitize('name').escape().trim(),
-            quantity: req.sanitize('quantity').escape().trim(),
+            qty: req.sanitize('quantity').escape().trim(),
             amount: req.sanitize('amount').escape().trim()
         }
-        
+
+        console.log(Item);
+
         req.getConnection(function(error, conn) {
             conn.query('INSERT INTO items SET ?', Item, function(err, result) {
                 if (err) {
                     req.flash('error', err)
-                    
+
                     res.render('items/add', {
                         title: 'Add New Item',
                         name: Item.name,
-                        quantity: Item.quantity,
-                        amount: Item.amount                    
+                        quantity: Item.qty,
+                        amount: Item.amount
                     })
-                } else {                
+                } else {
                     req.flash('success', 'Data added successfully!')
-                    
+
                     res.render('items/add', {
                         title: 'Add New Item',
                         name: '',
                         quantity: '',
-                        amount: ''                    
+                        amount: ''
                     })
                 }
             })
         })
     }
-    else {   
+    else {
         var error_msg = ''
         errors.forEach(function(error) {
             error_msg += error.msg + '<br>'
-        })                
-        req.flash('error', error_msg)        
-        
- 
-        res.render('items/add', { 
+        })
+        req.flash('error', error_msg)
+
+
+        res.render('items/add', {
             title: 'Add New Item',
             name: req.body.name,
             quantity: req.body.quantity,
@@ -87,49 +91,50 @@ app.post('/add', function(req, res, next){
         })
     }
 })
- 
+
 app.get('/edit/(:id)', function(req, res, next){
     req.getConnection(function(error, conn) {
         conn.query('SELECT * FROM items WHERE id = ' + req.params.id, function(err, rows, fields) {
             if(err) throw err
-            
+
             if (rows.length <= 0) {
                 req.flash('error', 'Item not found with id = ' + req.params.id)
                 res.redirect('/items')
             }
-            else { 
+            else {
                 res.render('items/edit', {
-                    title: 'Edit Item', 
+                    title: 'Edit Item',
                     id: rows[0].id,
                     name: rows[0].name,
                     quantity: rows[0].quantity,
-                    amount: rows[0].amount                    
+                    amount: rows[0].amount
                 })
-            }            
+            }
         })
     })
 })
- 
-app.put('/edit/(:id)', function(req, res, next) {
-    req.assert('name', 'Name is required').notEmpty()  
-    req.assert('quantity', 'quantity is required').notEmpty() 
-    req.assert('amount', 'A valid amount is required').isEmail()
- 
+
+app.post('/edit/(:id)', function(req, res, next) {
+    req.assert('name', 'Name is required').notEmpty()
+    req.assert('quantity', 'quantity is required').notEmpty()
+    req.assert('amount', 'A valid amount is required').notEmpty()
+
     var errors = req.validationErrors()
-    
-    if( !errors ) {   
+
+    if( !errors ) {
         var Item = {
             name: req.sanitize('name').escape().trim(),
-            quantity: req.sanitize('quantity').escape().trim(),
+            qty: req.sanitize('quantity').escape().trim(),
             amount: req.sanitize('amount').escape().trim()
         }
-        
+
         req.getConnection(function(error, conn) {
             conn.query('UPDATE items SET ? WHERE id = ' + req.params.id, Item, function(err, result) {
                 if (err) {
-                    req.flash('error', err)
-                    
-                    res.render('Item/edit', {
+                    console.log('error', error)
+                    req.flash('error', error)
+
+                    res.render('items/edit', {
                         title: 'Edit Item',
                         id: req.params.id,
                         name: req.body.name,
@@ -138,7 +143,7 @@ app.put('/edit/(:id)', function(req, res, next) {
                     })
                 } else {
                     req.flash('success', 'Data updated successfully!')
-                    
+
                     res.render('items/edit', {
                         title: 'Edit Item',
                         id: req.params.id,
@@ -150,26 +155,26 @@ app.put('/edit/(:id)', function(req, res, next) {
             })
         })
     }
-    else { 
+    else {
         var error_msg = ''
         errors.forEach(function(error) {
             error_msg += error.msg + '<br>'
         })
         req.flash('error', error_msg)
-  
-        res.render('items/edit', { 
-            title: 'Edit Item',            
-            id: req.params.id, 
+
+        res.render('items/edit', {
+            title: 'Edit Item',
+            id: req.params.id,
             name: req.body.name,
             quantity: req.body.quantity,
             amount: req.body.amount
         })
     }
 })
- 
+
 app.delete('/delete/(:id)', function(req, res, next) {
     var Item = { id: req.params.id }
-    
+
     req.getConnection(function(error, conn) {
         conn.query('DELETE FROM items WHERE id = ' + req.params.id, Item, function(err, result) {
             if (err) {
